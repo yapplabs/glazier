4c3c794f24d8bfd73d6f86b2a8113341307c3ec7
Ray Cohen <raycohen@gmail.com>
2013-05-22 19:26:22 -0400

    Use the github access token to retrieve the user's info

--- glazier/main.js
+++ glazier/main.js
@@ -1,6 +1,7 @@
 import ApplicationView from 'glazier/views/application';
+import ApplicationController from 'glazier/controllers/application';
 
 var Glazier = Ember.Application.create();
 Glazier.ApplicationView = ApplicationView;
-
+Glazier.ApplicationController = ApplicationController;
 export Glazier;
--- glazier/views/application.js
+++ glazier/views/application.js
@@ -1,4 +1,31 @@
 var ApplicationView = Ember.View.extend({
+  didInsertElement: function() {
+    var GITHUB_CLIENT_ID = Ember.$('meta[name=github_client_id]').attr('content');
+    var view = this;
+    document.getElementById('github_button').addEventListener('click', function(){
+      var githubUri = "https://github.com/login/oauth/authorize?scope=gist" +
+        "&client_id=" +  GITHUB_CLIENT_ID;
+      window.open(githubUri, "authwindow", "menubar=0,resizable=1,width=960,height=410");
+      return false;
+    });
+
+    window.addEventListener("message", function(event) {
+      if (event.origin !== window.location.origin) {
+        Ember.Logger.debug("got unknown message", event);
+        return;
+      }
+      var authCode = event.data;
+      Ember.Logger.debug("we got a code " + authCode);
+      Ember.$.ajax({
+        type: 'post',
+        url: window.location.origin + "/api/oauth/github/exchange?code=" + authCode
+      }).then(function(data) {
+        var accessToken = data;
+        view.set('controller.githubAccessToken', accessToken);
+        Ember.Logger.debug("My access token is ", accessToken);
+      });
+    });
+  }
 });
 
 export ApplicationView;
