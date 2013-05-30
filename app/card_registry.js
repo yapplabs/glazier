377f4e5fd5777f31c2613a6c8b2ad5b4e3a4df7e
Kris Selden <kris.selden@gmail.com>
2013-05-30 07:35:34 -0700

    move ProxyService back to its own file

--- app/card_registry.js
+++ app/card_registry.js
@@ -15,80 +15,18 @@ var services = {
   'github:authenticated:read' : '/cards/github-auth/manifest.json'
 };
 
-// tracks loaded cards
-var loaded = {};
-
-function Proxy(registry, port, name) {
-  this.registry = registry;
-  this.port = port;
-  this.name = name;
-  this.loaded = false;
-  this.targetPromise = new Conductor.Oasis.RSVP.Promise();
-}
-
-Proxy.prototype = {
-  load: function () {
-    if (this.loaded) return;
-    console.log('Proxy.load target for '+this.name);
-    var self = this;
-    this.registry.resolveService(this.name).then(function (target) {
-      console.log('Proxy target loaded for '+self.name);
-      self.targetPromise.resolve(target);
-    });
-    this.loaded = true;
-  },
-  handle: function (eventName, data) {
-    var self = this;
-    console.log('Proxy handle '+ eventName);
-    this.load(); //lazy load service
-    this.targetPromise.then(function (target) {
-      console.log('Proxy forwarding '+ eventName);
-      var responseEvent = eventName.replace(/^@request:/, '@response:');
-      var requestId = data.requestId;
-
-      var observer = function(event) {
-        if (event.requestId === requestId) {
-          console.log('Proxy response '+ responseEvent);
-          target.port.off(responseEvent, observer);
-          this.port.send(responseEvent, event);
-        }
-      };
-
-      target.port.on(responseEvent, observer, self);
-      target.port.send(eventName, data);
-    }, function (e) {
-      console.error(e);
-    });
-  }
-};
+import ProxyService from 'glazier/services/proxy';
 
 function CardRegistry(conductor) {
   this.conductor = conductor;
   var cardId, manifest;
 
-  var ProxyService = Conductor.Oasis.Service.extend({
-    registry: this,
-    initialize: function (port, name) {
-      // this.sandbox.card.id
-      // if name in consumes then this is the requester
-      // if name in provides then this is the target
-      // assert card can't provide and consume the same service
-      var card = this.sandbox.card, cardId = card.id;
-      var manifest = manifests[cardId];
-      if (manifest.provides && manifest.provides.indexOf(name) !== -1) {
-        if (!card.proxyTargets) {
-          card.proxyTargets = {};
-        }
-        card.proxyTargets[name] = this;
-      } else {
-        var proxy = new Proxy(this.registry, this.port, name);
-        this.port.all(proxy.handle, proxy);
-      }
-    }
+  var Service = ProxyService.extend({
+    registry: this
   });
 
   function registerService(name) {
-    conductor.services[name] = ProxyService;
+    conductor.services[name] = Service;
   }
 
   for (cardId in manifests) {
@@ -100,7 +38,17 @@ function CardRegistry(conductor) {
   }
 }
 
+
+// tracks loading promises
+var loaded = {};
 CardRegistry.prototype = {
+  isProvider: function (cardId, serviceName) {
+    var manifest = manifests[cardId];
+    if (manifest.provides) {
+      return manifest.provides.indexOf(serviceName) !== -1;
+    }
+    return false;
+  },
   load: function (manifestUrl) {
     var promise = loaded[manifestUrl];
     if (!promise) {
@@ -135,7 +83,6 @@ CardRegistry.prototype = {
     }
     return promise;
   },
-
   resolveService: function (serviceName) {
     var cardId = services[serviceName]; // 1 to 1 for now
     return this.load(cardId).then(function (card) {
