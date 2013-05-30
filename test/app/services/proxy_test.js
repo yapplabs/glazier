7f5f876b2dc9b328bfaf55962068942b7200fa04
Stefan Penner <stefan.penner@gmail.com>
2013-05-30 16:40:00 -0400

    WIP

--- test/app/services/proxy_test.js
+++ test/app/services/proxy_test.js
@@ -34,32 +34,104 @@ asyncTest('test ProxyService for a consumer', 9, function () {
   var responseEvent = {
     requestId: 'request id'
   };
-  var srcPort = {
-    all: function (callback, binding) {
-      this._callback = callback;
-      this._binding = binding;
+
+  function MockPort() { }
+
+  function logCall(fn, args) {
+    fn.calledWith = fn.calledWith || [];
+    fn.calledWith.push(Array.prototype.slice.call(arguments));
+  }
+
+  MockPort.prototype = {
+    _events: { },
+    _all: [ ],
+    all: function all(callback, binding){
+      logCall(all, arguments);
+
+      this._all.push([callback, binding]);
     },
-    send: function (name, event) {
-      equal(name, responseEventName, 'proxy sends response back to src port');
-      equal(event, responseEvent, 'proxy sends response back to src port');
-      start();
+
+    on: function on(eventName, callback, binding) {
+      logCall(on, arguments);
+
+      this._events[eventName] = this._events[eventName] || [];
+      this._events[eventName].push([callback, binding]);
+    },
+
+    off: function off(eventName) {
+      logCall(off, arguments);
+
+      delete this._events[eventName];
+    },
+
+    // this is sending to an iframe, e.g postMessage
+    send: function send(name, event){
+      logCall(send, arguments);
+      this.port.trigger(name, event);
+    },
+
+    // this will be used from the test, to mimic
+    // an iframe posting back
+    //
+    // e.g emitting message event
+    trigger: function(name, event) {
+      var port = this;
+
+      function processEvents() {
+        port._all.forEach(function(tuple) {
+          var callback = tuple[0];
+          var binding = tuple[1];
+
+          callback.call(binding, name, event);
+        });
+
+        var tuples = port._events[name] || [];
+
+        tuples.forEach(function(tuple) {
+          var callback = tuple[0];
+          var binding = tuple[1];
+
+          callback.call(binding, event);
+        });
+      }
+
+      setTimeout(processEvents, 0);
     }
   };
-  var targetPort = {
-    on: function (eventName, callback, binding) {
-      equal(eventName, responseEventName, 'proxy subscribed to responseEvent');
-      this._callback = callback;
-      this._binding = binding;
-    },
-    off: function () {},
-    send: function (name, event) {
-      equal(name, requestEventName, 'target got forwarded event');
-      equal(event, requestEvent, 'target got forwarded event');
-      setTimeout(function () {
-        targetPort._callback.call(targetPort._binding, responseEvent);
-      }, 0);
-    }
-  };
+
+  function mockChannel(portA, portB) {
+    this.portA = portA;
+    this.portb = portb;
+
+    portA.port = portB;
+    portB.port = portA;
+  }
+
+  var srcPort = new MockPort();
+  var targetPort = new MockPort();
+
+  var channel = mockChannel(srcPort, targetPort);
+
+  //srcPort.send = function (name, event) {
+  //  equal(name, responseEventName, 'proxy sends response back to src port');
+  //  equal(event, responseEvent, 'proxy sends response back to src port');
+  //  start();
+  //};
+
+  //targetPort.on = function (eventName, callback, binding) {
+  //    equal(eventName, responseEventName, 'proxy subscribed to responseEvent');
+  //    this._callback = callback;
+  //    this._binding = binding;
+  //};
+
+  //targetPort.send = function (name, event) {
+  //    equal(name, requestEventName, 'target got forwarded event');
+  //    equal(event, requestEvent, 'target got forwarded event');
+  //    setTimeout(function () {
+  //      targetPort._callback.call(targetPort._binding, responseEvent);
+  //    }, 0);
+  //  }
+  //};
 
   var targetService = {
     port: targetPort
@@ -84,14 +156,18 @@ asyncTest('test ProxyService for a consumer', 9, function () {
       }
     }
   });
+
   var service = new Service(srcPort, sandbox);
 
   ok(service, 'created service');
 
   service.initialize(srcPort, capability);
 
+  // card | srcPort <-> proxy <-> [targetPort | card]
+
   equal(srcPort._callback, service.forward, 'service subscribed to port');
   equal(srcPort._binding, service, 'service subscribed to port');
 
-  srcPort._callback.call(srcPort._binding, requestEventName, requestEvent);
+  // simulate the message event fromthe source ports message channel
+  srcPort.trigger(requestEventName, requestEvent);
 });
