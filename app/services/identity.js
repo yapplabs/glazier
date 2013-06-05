b372fbd79c94198a42853fe08e15484d1c0649a3
Stefan Penner <stefan.penner@gmail.com>
2013-06-05 14:21:57 -0400

    test and refactor the Identity service

--- app/services/identity.js
+++ app/services/identity.js
@@ -1,19 +1,32 @@
 
 var IdentityService = Conductor.Oasis.Service.extend({
+  updateName: function(name){
+
+    // TODO: not this
+    Ember.run(function() {
+      Glazier.__container__.lookup("controller:application").set('name', name);
+    });
+  },
   events: {
-    identified: function(userData) {
+    oauthIdentityEstablished: function(oauthData) {
       var service = this,
           data = {
-            github_access_token: userData.githubAccessToken
+            github_access_token: oauthData.accessToken
           };
+
+      function broadcast(userJson) {
+        service.updateName(userJson.user.github_login);
+        service.port.send('identified', userJson);
+      }
+
       $.ajax({
         url: '/api/session.json',
         type: 'POST',
         data: data
-      }).then(function(userJson){
-        Glazier.__container__.lookup("controller:application").set('name', userJson.user.github_login);
-        service.port.send('identified', userJson);
-      }, console.error);
+      }).then(broadcast).then(null,function(reason) {
+        console.error(reason, reason.message, reason.stack);
+        throw reason;
+      });
     }
   }
 });
--- test/helpers/service_test_helpers.js
+++ test/helpers/service_test_helpers.js
@@ -14,13 +14,26 @@ function createServiceForTesting(ServiceClass, cardId) {
   var service = new ServiceClass(port, sandbox);
 
   service.simulateRequest = function(requestName, key, value) {
-    var promise = new Conductor.Oasis.RSVP.Promise();
-    this.requests[requestName].apply(this, [promise, key, value]);
+    var promise = new Conductor.Oasis.RSVP.Promise(),
+    handler = this.requests[requestName];
+
+    if (!handler) {
+      throw new Error("No such Request: `" + requestName + "`");
+    }
+
+    handler.call(this, promise, key, value);
+
     return promise;
   };
 
   service.simulateSend = function(eventName, data) {
-    this.events[eventName].apply(this, [eventName, data]);
+    var handler = this.events[eventName];
+
+    if (!handler) {
+      throw new Error("No such Event: `" + eventName + "`");
+    }
+
+    handler.call(this, data);
   };
 
   return service;
