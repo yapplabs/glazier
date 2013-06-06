7a2638dfd67acfabde416eda18e3febec4d065ea
Luke Melia <luke@lukemelia.com>
2013-06-06 15:08:09 -0400

    Fixed watch task (find-and-replace gone wrong)

--- configurations/watch.js
+++ configurations/watch.js
@@ -1,7 +1,7 @@
 module.exports = {
   scripts: {
     files: ['app/**', 'cards/**', 'vendor/**', 'test/**',  'templates/**'],
-    Configs: ['lock', 'build', 'unlock', 'jshint', 'qunit:all'],
+    tasks: ['lock', 'build', 'unlock', 'jshint', 'qunit:all'],
     options: {
       nospawn: true
     }
