c4a6f80a3deec2f035627432493f5517a1f5c7c2
Luke Melia <luke@lukemelia.com>
2013-06-06 18:52:00 -0400

    Update jshint config to avoid complaints for acceptable use

--- configurations/jshint.js
+++ configurations/jshint.js
@@ -1,7 +1,13 @@
 module.exports = {
   all: {
     // TODO: Run jshint on individual files when jshint supports ES6 modules
-    src: ['Gruntfile.js', 'tmp/public/**/*.js', '!tmp/public/vendor/**', '!tmp/public/test.js'],
+    src: [
+      'Gruntfile.js',
+      'tmp/public/**/*.js',
+      '!tmp/public/vendor/**',
+      '!tmp/public/test.js',
+      '!tmp/public/cards/**'
+    ],
     options: {
       jshintrc: '.jshintrc',
       force: true
