5b2aa5dc38f47bdfb12aea738b2abcc5dbe3746b
Stefan Penner <stefan.penner@gmail.com>
2013-06-07 15:39:54 -0400

    resolver

--- cards/github-issues/app/application.js
+++ cards/github-issues/app/application.js
@@ -1,9 +1,30 @@
 var loadEmberApp = function() {
-  var App = Ember.Application.create({
-    rootElement: '#card'
-  });
 
-  App.ApplicationController = requireModule('app/controllers/application');
+  function resolve(prefix) {
+    return function(parsedName) {
+      var moduleName = prefix + parsedName.fullNameWithoutType;
+
+      if (define.registry[moduleName]) {
+        console.log('Module hit:', moduleName);
+        return requireModule(moduleName);
+      } else {
+        console.log('Module mis:', moduleName);
+        return this._super(parsedName);
+      }
+    };
+  }
+
+  function Resolver(prefix) {
+    return Ember.DefaultResolver.extend({
+      resolveController: resolve(prefix + '/controllers/'),
+      resolveView:       resolve(prefix + '/views/')
+    });
+  }
+
+  var App = Ember.Application.create({
+    rootElement: '#card',
+    resolver: Resolver('app')
+  });
 
   App.deferReadiness();
   requireModule('templates');
