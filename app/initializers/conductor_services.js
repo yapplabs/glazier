55aa10d6316493f7cbf779dd6a687ad97d5bcc7c
Kris Selden <kris.selden@gmail.com>
2013-06-07 13:32:33 -0700

    make service initializer register all container keys of type service

--- app/initializers/conductor_services.js
+++ app/initializers/conductor_services.js
@@ -1,12 +1,13 @@
 var initializer = {
   name: 'conductorServices',
   initialize: function (container, application) {
-    Conductor.services.configuration = container.lookup('service:configuration');
-    Conductor.services.repository = container.lookup('service:repository');
-    Conductor.services.fullXhr = container.lookup('service:fullXhr');
-    Conductor.services.userStorage = container.lookup('service:userStorage');
-    Conductor.services.identity = container.lookup('service:identity');
-    Conductor.services.login = container.lookup('service:login');
+    container.registry.eachLocal(function (key) {
+      var match = /^service:(.+)/.exec(key);
+      if (match) {
+        var keyWithoutType = match[1];
+        Conductor.services[keyWithoutType] = container.lookup(key);
+      }
+    });
   }
 };
 
