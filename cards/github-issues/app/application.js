5812b353c4a631eaebcbc03c0612734f1cb348dc
Stefan Penner <stefan.penner@gmail.com>
2013-06-12 11:42:06 -0400

    use the same resolver in the issues card

--- cards/github-issues/app/application.js
+++ cards/github-issues/app/application.js
@@ -1,23 +1,54 @@
 var loadEmberApp = function() {
 
+  var typeMap = {
+    view: 'views',
+    util: 'utils',
+    route: 'routes',
+    service: 'services',
+    controller: 'controllers'
+  };
+
+  function classFactory(klass) {
+    return {
+      create: function (injections) {
+        return klass.extend(injections);
+      }
+    };
+  }
+
   function resolve(prefix) {
-    return function(parsedName) {
-      var moduleName = prefix + parsedName.fullNameWithoutType;
+    return function(parsedName){
+      var pluralizedType = typeMap[parsedName.type] || parsedName.type;
+      var name = Ember.String.underscore(parsedName.fullNameWithoutType);
+
+      var moduleName = prefix + '/' +  pluralizedType + '/' + name;
+      var module;
 
       if (define.registry[moduleName]) {
-        console.log('Module hit:', moduleName);
-        return requireModule(moduleName);
-      } else {
-        console.log('Module miss:', moduleName);
+        module = requireModule(moduleName);
+
+        if (typeof module.create !== 'function') {
+          module = classFactory(module);
+        }
+
+        if (Ember.ENV.LOG_MODULE_RESOLVER){
+          console.log('hit', moduleName);
+        }
+
+        return module;
+      } else  {
+        if (Ember.ENV.LOG_MODULE_RESOLVER){
+          console.log('miss', moduleName);
+        }
+
         return this._super(parsedName);
       }
     };
   }
 
   function resolver(prefix) {
-    return Ember.DefaultResolver.extend({
-      resolveController: resolve(prefix + '/controllers/'),
-      resolveView:       resolve(prefix + '/views/')
+    return  Ember.DefaultResolver.extend({
+      resolveOther: resolve(prefix)
     });
   }
 
