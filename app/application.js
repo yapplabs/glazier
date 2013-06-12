6c181198a43ed71809454ad4cbcddd5558186f06
Luke Melia <luke@lukemelia.com>
2013-06-12 14:19:36 -0400

    Extract resolver to it's own file

--- app/application.js
+++ app/application.js
@@ -1,68 +1,11 @@
 import Router from 'glazier/router';
 import Store from 'glazier/store';
-
-var typeMap = {
-  view: 'views',
-  util: 'utils',
-  route: 'routes',
-  service: 'services',
-  controller: 'controllers'
-};
-
-function classFactory(klass) {
-  return {
-    create: function (injections) {
-      if (typeof klass.extend === 'function') {
-        return klass.extend(injections);
-      } else {
-        return klass;
-      }
-    }
-  };
-}
-
-var underscore = Ember.String.underscore;
-function resolve(prefix) {
-  return function(parsedName){
-
-    var pluralizedType = typeMap[parsedName.type] || parsedName.type;
-    var name = parsedName.fullNameWithoutType;
-
-    var moduleName = prefix + '/' +  pluralizedType + '/' + underscore(name);
-    var module;
-
-    if (define.registry[moduleName]) {
-      module = requireModule(moduleName);
-
-      if (typeof module.create !== 'function') {
-        module = classFactory(module);
-      }
-
-      if (Ember.ENV.LOG_MODULE_RESOLVER){
-        console.log('hit', moduleName);
-      }
-
-      return module;
-    } else  {
-      if (Ember.ENV.LOG_MODULE_RESOLVER){
-        console.log('miss', moduleName);
-      }
-
-      return this._super(parsedName);
-    }
-  };
-}
-
-function resolver(prefix) {
-  return  Ember.DefaultResolver.extend({
-    resolveOther: resolve(prefix)
-  });
-}
+import Resolver from 'glazier/resolver';
 
 var Application = Ember.Application.extend({
   Router: Router,
   Store: Store,
-  resolver: resolver('glazier')
+  resolver: Resolver
 });
 
 export Application;
---
+++ app/resolver.js
@@ -0,0 +1,57 @@
+var typeMap = {
+  view: 'views',
+  util: 'utils',
+  route: 'routes',
+  service: 'services',
+  controller: 'controllers'
+};
+
+function classFactory(klass) {
+  return {
+    create: function (injections) {
+      if (typeof klass.extend === 'function') {
+        return klass.extend(injections);
+      } else {
+        return klass;
+      }
+    }
+  };
+}
+
+var underscore = Ember.String.underscore;
+function resolve(prefix) {
+  return function(parsedName){
+
+    var pluralizedType = typeMap[parsedName.type] || parsedName.type;
+    var name = parsedName.fullNameWithoutType;
+
+    var moduleName = prefix + '/' +  pluralizedType + '/' + underscore(name);
+    var module;
+
+    if (define.registry[moduleName]) {
+      module = requireModule(moduleName);
+
+      if (typeof module.create !== 'function') {
+        module = classFactory(module);
+      }
+
+      if (Ember.ENV.LOG_MODULE_RESOLVER){
+        console.log('hit', moduleName);
+      }
+
+      return module;
+    } else  {
+      if (Ember.ENV.LOG_MODULE_RESOLVER){
+        console.log('miss', moduleName);
+      }
+
+      return this._super(parsedName);
+    }
+  };
+}
+
+var Resolver = Ember.DefaultResolver.extend({
+  resolveOther: resolve('glazier')
+});
+
+export Resolver;
