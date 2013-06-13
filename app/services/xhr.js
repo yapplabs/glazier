9b4d631a290384286394162bdfccab917e0902d8
Ray Cohen <raycohen@gmail.com>
2013-06-13 15:50:20 -0400

    extend and replace the XHRService with our ManifestXHRService which can take manifests into account

---
+++ app/services/xhr.js
@@ -0,0 +1,51 @@
+// TODO - get these from somewhere
+var cardManifests = {
+  'card1_id': {'assets': {'card1.css': 'http://full-path-to/card1-fingerprint.css'}}
+};
+var glazierUrlMap = {
+  '/some/vendor/path.js': 'expanded path for /some/vendor/path.js'
+};
+
+// Lookup a url in the manifest for this card
+var cardManifestUrl = function(cardId, url) {
+  var manifest = cardManifests[cardId];
+  if (manifest && manifest.assets && manifest.assets[url]) {
+    return manifest.assets[url];
+  }
+};
+
+// Lookup a url in the manifest available to all cards
+var glazierUrl = function(url) {
+  if (glazierUrlMap[url]) return glazierUrlMap[url];
+};
+
+var ManifestXHRService = Conductor.XHRService.extend({
+  requests: {
+    /*
+      @public
+
+      Extend Conductor's XHRService to allow checking urls against a manifest so that cards
+      can map non-fingerprinted urls to fingerprinted urls.
+
+      Also alow glazier to provide fingerprinted urls for vendored assets that it uses to cards.
+
+      By setting `Conductor.services['xhr'] = ManifestXHRService` this will take the place of the original XHRService.
+
+      @method get
+      @param promise {Conductor.Oasis.RSVP.Promise}
+      @param url {String}
+    */
+    get: function(promise, url) {
+      var processedUrl = cardManifestUrl(this.sandbox.card.id, url) || glazierUrl(url) || url;
+
+      if (url !== processedUrl) {
+        console.log("ManifestXHRService get " + url + " -> " + processedUrl);
+      }
+
+      var xhrServiceGet = Conductor.XHRService.prototype.requests.get;
+      return xhrServiceGet.apply(this, [promise, processedUrl]);
+    }
+  }
+});
+
+export = ManifestXHRService;
