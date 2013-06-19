9a55f155fe7a07bbde576e1322ca9bd2354c93b1
Ray Cohen <raycohen@gmail.com>
2013-06-19 15:50:07 -0400

    update use of glazier-card-grunt-config

--- cards/github-repositories/Gruntfile.js
+++ cards/github-repositories/Gruntfile.js
@@ -1,19 +1,9 @@
 module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-  var _ = require('underscore'),
-    sharedConfig = require('glazier-card-grunt-config').config;
 
-  var config = {
-    pkg: grunt.file.readJSON('package.json'),
-    env: process.env
-  };
+  var glazierCardGruntConfig = require('glazier-card-grunt-config'),
+    sharedConfig = glazierCardGruntConfig.createSharedConfig(grunt);
 
-  config = _.extend(config, sharedConfig);
-  grunt.initConfig(config);
-
-  grunt.registerTask('build', ['clean', 'ember_handlebars', 'transpile', 'jshint', 'copy:main', 'concat']);
-  grunt.registerTask('manifest', ['build', 'md5', 'copy:manifest']);
-  grunt.registerTask('deploy', ['manifest', 's3']);
-
-  grunt.registerTask('default', ['build']);
+  grunt.initConfig(sharedConfig);
+  glazierCardGruntConfig.registerSharedTasks(grunt);
 };
