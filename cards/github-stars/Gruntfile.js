bd079a78f1564b78a1ad853faab05545202d8224
Ray Cohen <raycohen@gmail.com>
2013-06-19 15:21:00 -0400

    use the shared grunt config in all the cards

--- cards/github-stars/Gruntfile.js
+++ cards/github-stars/Gruntfile.js
@@ -1,22 +1,15 @@
 module.exports = function(grunt) {
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  var _ = require('underscore'),
+    sharedConfig = require('glazier-card-grunt-config').config;
 
-  function config(configFileName) {
-    return require('./configurations/' + configFileName);
-  }
-
-  grunt.initConfig({
+  var config = {
     pkg: grunt.file.readJSON('package.json'),
-    env: process.env,
-    s3: config('s3'),
-    md5: config('md5'),
-    copy: config('copy') ,
-    clean: ["tmp", "dist"],
-    concat: config('concat'),
-    jshint: config('jshint'),
-    transpile: config('transpile'),
-    ember_handlebars: config('ember_handlebars')
-  });
+    env: process.env
+  };
+
+  config = _.extend(config, sharedConfig);
+  grunt.initConfig(config);
 
   grunt.registerTask('build', ['clean', 'ember_handlebars', 'transpile', 'jshint', 'copy:main', 'concat']);
   grunt.registerTask('manifest', ['build', 'md5', 'copy:manifest']);
