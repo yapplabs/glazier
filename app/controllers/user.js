a527ec3cce185cf25b59c1c0fb8561dfae923ebc
Stefan Penner <stefan.penner@gmail.com>
2013-06-20 17:59:19 -0400

    share a run-loop-aware rsvp'd ajax

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,3 +1,5 @@
+import 'glazier/utils/ajax' as ajax;
+
 var UserController = Ember.Controller.extend({
   content: null,
   isLoggedIn: Ember.computed.bool('content'),
@@ -12,9 +14,10 @@ var UserController = Ember.Controller.extend({
   }.property(),
   exchangeGithubOauthCode: function(authCode){
     var self = this;
-    Ember.$.ajax({
+
+    ajax(
+      "/api/oauth/github/exchange?code=" + authCode, {
       type: 'post',
-      url: "/api/oauth/github/exchange?code=" + authCode
     }).then(function(accessToken) {
       self.loginWithGithub(accessToken);
     }).then(null, Conductor.error);
@@ -25,16 +28,13 @@ var UserController = Ember.Controller.extend({
   },
   loginWithGithub: function(githubAccessToken) {
     var self = this;
-    return Ember.$.ajax({
-      url: '/api/session.json',
+    return ajax('/api/session.json', {
       type: 'POST',
       data: {
         github_access_token: githubAccessToken
       }
     }).then(function(data){
-      Ember.run(function(){
-        self.set('content', data.user);
-      });
+      self.set('content', data.user);
     }).then(null, Conductor.error);
   },
   setCurrentUser: function (user) {
---
+++ app/utils/ajax.js
@@ -0,0 +1,15 @@
+function ajax(url, options){
+  return Ember.RSVP.Promise(function(resolve, reject){
+    options.success = function(data) {
+      Ember.run(null, resolve, data);
+    };
+
+    options.error = function() {
+      Ember.run(null, reject, arguments);
+    };
+
+    $.ajax(url, options);
+  });
+}
+
+export = ajax;
