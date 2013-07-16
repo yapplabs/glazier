d57536d5cd9bd005a7e93ee9f2f08de58087e832
Scott Polhemus <scott.polhemus@barrelny.com>
2013-07-16 11:43:52 -0400

    Modified github-stars card
    - Show usernames in tooltips
    - Find total number of stargazers using GitHub API pagination

--- cards/github-stars/app/routes/application.js
+++ cards/github-stars/app/routes/application.js
@@ -9,7 +9,30 @@ var Stargazers = {
     var apiConsumer = Stargazers.getApiConsumer(user);
     return apiConsumer.request("ajax", {
       url: '/repos/' + repositoryName + '/stargazers?sort=updated',
+      dataType: 'jsonp'
+    }).then(function(stargazers) {
+      if(stargazers.meta.Link) {
+        var lastPageUrl = stargazers.meta.Link[1][0];
+        return Stargazers.getTotal(lastPageUrl, user).then(function(total) {
+          return {
+            data: stargazers.data,
+            total: total
+          };
+        });
+      } else {
+        return stargazers;
+      }
+    });
+  },
+  getTotal: function(last, user) {
+    var apiConsumer = Stargazers.getApiConsumer(user);
+    var url = last.slice(last.indexOf('/repos'), last.indexOf('&callback')) + last.slice(last.indexOf('&page'));
+    return apiConsumer.request("ajax", {
+      url: url,
       dataType: 'json'
+    }).then(function(lastPage) {
+      var pages = Number(url.slice(url.indexOf('page=')+5));
+      return (pages-1) * 30 + (lastPage.length);
     });
   },
   currentUserStarred: function(repositoryName, user) {
@@ -63,8 +86,9 @@ function retrieveStargazers(user) {
       return {
         user: user,
         repositoryName: repositoryName,
-        stargazers: allResults[0],
-        isStarred: allResults[1]
+        stargazers: allResults[0].data,
+        totalStargazers: allResults[0].total || allResults[0].data.length,
+        isStarred: allResults[1],
       };
     });
   }).then(null, Conductor.error);
