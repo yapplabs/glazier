9571d4ba7bdc836fcdeed5b679860a00bdb6adc0
Kris Selden <kris.selden@gmail.com>
2013-07-18 14:58:34 -0700

    fix adding editable repositories to user data

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,15 +1,14 @@
 var ApplicationRoute = Ember.Route.extend({
   beforeModel: function(){
-    debugger;
     var userId = getUserIdFromCookie();
     if (!userId) { return; } // no cookie yet - not logged in
 
     var userController = this.controllerFor('user');
     return Ember.$.ajax({
-      url: '/api/users/' + userId,
+      url: '/api/user',
       dataType: 'json'
-    }).then(function(userJson) {
-      userController.set('content', user);
+    }).then(function(data) {
+      userController.set('content', data.user);
     });
   }
 });
@@ -20,7 +19,7 @@ function getUserIdFromCookie(){
     var cookie = cookies[i],
         match = /^login=.+?\-(.+)/.exec(cookie);
     if (match) {
-      return JSON.parse(decodeURIComponent(match[1])).id;
+      return JSON.parse(decodeURIComponent(match[1])).github_id;
     }
   }
   return null;
