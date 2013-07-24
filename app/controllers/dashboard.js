9282cf2765383fa8233bd4bb72dddb0b17997df7
Ray Cohen <raycohen@gmail.com>
2013-07-24 11:28:50 -0400

    add pane link

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,2 +1,26 @@
-var DashboardController = Ember.ObjectController.extend({});
+var DashboardController = Ember.ObjectController.extend({
+  needs: ['user'],
+  user: Ember.computed.alias('controllers.user'),
+  repositoryName: Ember.computed.alias('id'),
+
+  canAddPanes: function() {
+    return this.get('isAdmin') && !this.get('addingPane');
+  }.property('isAdmin', 'addingPane'),
+
+  isAdmin: function() {
+    var user = this.get('user.content'),
+      repositoryName = this.get('repositoryName'),
+      repos = user && user.editable_repositories;
+    return repos && repos.indexOf(repositoryName) !== -1;
+  },
+
+  addablePanes: function() {
+    return Glazier.PaneType.all();
+  }.property(),
+
+  addingPane: false,
+  addPane: function() {
+    this.toggleProperty('addingPane');
+  }
+});
 export default DashboardController;
