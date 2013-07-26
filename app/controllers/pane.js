a3b570bc53246b5d7e42bc964da22d68fd9c4133
Stefan Penner <stefan.penner@gmail.com>
2013-07-26 12:19:19 -0400

    restore isAdmin

--- app/controllers/pane.js
+++ app/controllers/pane.js
@@ -2,7 +2,7 @@ var PaneController = Ember.ObjectController.extend({
   needs: ['dashboard'],
   isAdmin: Ember.computed.alias('controllers.dashboard.isAdmin'),
   isHidden: false,
-  card: null
+  card: null,
 });
 
 export default PaneController;
