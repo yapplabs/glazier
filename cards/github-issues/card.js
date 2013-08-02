21e2e4f51cd6568e07f594a31ab0421afda3b634
Stefan Penner <stefan.penner@gmail.com>
2013-08-02 13:23:26 -0400

    WIP

--- cards/github-issues/card.js
+++ cards/github-issues/card.js
@@ -21,8 +21,11 @@ var card = Conductor.card({
   App: null,
 
   render: function (intent, dimensions) {
-    document.body.innerHTML = "<div id=\"card\"></div>";
-    Ember.run(this.App, 'advanceReadiness');
+    if (!document.getElementById('card') ){
+      document.body.innerHTML = "<div id=\"card\"></div>";
+    }
+
+    return this.App.render(intent, dimensions);
   },
 
   activate: function() {
