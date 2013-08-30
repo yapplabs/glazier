96798c1fc3e4a8f77ea8a4ea90d84722514d5e7b
Luke Melia <luke@lukemelia.com>
2013-08-30 14:17:10 -0400

    ajax helper now returns an error object that is serializable across Oasis' postMessage boundary

--- app/utils/ajax.js
+++ app/utils/ajax.js
@@ -4,8 +4,16 @@ function ajax(url, options){
       Ember.run(null, resolve, data);
     };
 
-    options.error = function() {
-      Ember.run(null, reject, arguments);
+    options.error = function(jqXHR, textStatus, errorThrown) {
+      // Construct a serialization-friendly error object
+      //   This value should survive the postMessage transport used to communicate to/from cards
+      var error = {
+        type: textStatus,
+        status: jqXHR.status,
+        responseText: jqXHR.responseText,
+        headers: jqXHR.getAllResponseHeaders()
+      };
+      Ember.run(null, reject, error);
     };
 
     $.ajax(url, options);
