800a6b1665de73fe45aa75affc096d73824a415d
Stefan Penner <stefan.penner@gmail.com>
2013-10-15 12:05:41 -0400

    pretend the user is logged in

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -3,10 +3,11 @@ var DashboardController = Ember.ObjectController.extend({
   user: Ember.computed.alias('controllers.user'),
   repositoryName: Ember.computed.alias('id'),
   isAdmin: function() {
-    var user = this.get('user.content'),
-      repositoryName = this.get('repositoryName'),
-      repos = user && user.editable_repositories;
-    return repos && repos.indexOf(repositoryName) !== -1;
+    // var user = this.get('user.content'),
+    //   repositoryName = this.get('repositoryName'),
+    //   repos = user && user.editable_repositories;
+    // return repos && repos.indexOf(repositoryName) !== -1;
+    return true;
   }.property('user.content', 'repositoryName')
 });
 export default DashboardController;
